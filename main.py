ad4461e4c2fb20e09af6e28db1d71b249dbfa934
adn87 <aaghadieh@gmail.com>
2023-12-09 11:35:45 -0500

    My_Website_002
    - created the about page.

--- main.py
+++ main.py
@@ -11,5 +11,25 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
